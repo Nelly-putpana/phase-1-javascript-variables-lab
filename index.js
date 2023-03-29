76fc727aaf590ae2e95e873e89eee7637bcf5c27
Nelly Putpana <nelly.putpana@gmail.com>
2023-03-29 17:13:22 -0600

    variable lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName='Scuber'
+
+companyName;
+
+
+
+mostProfitableNeighborhood = 'Chelsea';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+companyCeo = 'Susan Smith';
+
+let companyCeo = 'Susan Smith';
+
